ee7d22b01dc400da6f42918a0a6fa0e1fd0a4a35
Vk <kinkarvansh@gmail.com>
2025-07-26 14:01:14 +0000

    init

---
+++ src/Raffle.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+/**
+ * @title Raffle contract
+ * @author Vk1033
+ * @notice This contract is for creating a sample raffle.
+ * @dev Implements Chainlink VRF for randomness.
+ */
+contract Raffle {}
